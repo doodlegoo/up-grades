f535138ae886e8ee6a3fb124a10b817d7aedd8e6
Vincent Gagnon <vincent.gagnon5@usherbrooke.ca>
2015-02-28 04:39:34 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
-$mongo = new MongoClient();
-$dbs = $mongo->listDBs();
-print_r($dbs);
+$mongo = new Mongo();
+$db = $mongo->subjects;
+$collection = $db->questions;
+
+$courses = $db->courses;
+$course = array(
+    'nom'=>'GIS113',
+    'owner' =>'Vince',
+    'university'=>'Sherbrooke'
+);
+$course_id = $courses->insert($course);
+print_r($db);
 ?>
