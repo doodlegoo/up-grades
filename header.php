1181860b4f056f968992d5e7c76217e5f5d952bf
doodlegoo <vincent.gagnon5@gmail.com>
2015-03-01 15:48:49 -0500

    styling this shit

--- header.php
+++ header.php
@@ -7,13 +7,13 @@ position:absolute; top:0px;left:0px; height : 100px; width:100%""/>
  		
  	<ul style="margin-left:2px;float:right;">
  		
-		<li style="margin-left:2px;float:left; margin-top: 90px;"><a style="text-decoration:none;color:black;" href="#">Rechercher cours</a></li>
-		<li style="margin-left:2px;float:left;text-decoration:none;color:black; margin-top: 90px; margin-left:30px;"><a style="text-decoration:none;color:black;" href="note.php">Mes notes de cours</a></li>
-		<li style="margin-left:2px;float:left;text-decoration:none;color:black; margin-top: 90px; margin-left:30px; margin-left: 25px;"><a style="text-decoration:none;color:black;" href="quiz.php">Quiz</a></li>
+		<li class="btn" style="margin-left:2px;float:left; margin-top: 90px;"><a style="text-decoration:none;color:black;" href="#">Rechercher cours</a></li>
+		<li class="btn" style="margin-left:2px;float:left;text-decoration:none;color:black; margin-top: 90px; margin-left:30px;"><a style="text-decoration:none;color:black;" href="note.php">Mes notes de cours</a></li>
+		<li class="btn" style="margin-left:2px;float:left;text-decoration:none;color:black; margin-top: 90px; margin-left:30px; margin-left: 25px;"><a style="text-decoration:none;color:black;" href="quiz.php">Quiz</a></li>
 		
 	</ul>
 	<div id="hautdroit" style="position:absolute ;margin-top: 5px; right:1px; top: 1px; width:100px;">
-	<a href="profile.php" style="text-decoration:none; color:black;" >John Smith</a>
+	<a class="btn" href="profile.php" style="text-decoration:none; color:black;" >Mon profil</a>
 	</div>
 	<link rel="stylesheet" href="css/style.css" />
 	<link rel="stylesheet" href="css/page.css" />
